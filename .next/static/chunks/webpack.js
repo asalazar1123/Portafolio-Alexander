52dfa22cc1173196662af303b2d0134520e7b82b
asalazar1123 <salazaralexander2@gmail.com>
2024-02-07 07:45:33 -0300

    correcciones

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "802150a7db152dda"; }
+/******/ 		__webpack_require__.h = function() { return "140193d1135d5d92"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/blog.js
+++ pages/blog.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 
 const blog = () => (
     <Layout>
-    <h1>Proximamente</h1>
+    <h1>Proximamente disponible</h1>
     </Layout>
 )
 
